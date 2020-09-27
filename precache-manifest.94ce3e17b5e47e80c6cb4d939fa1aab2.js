279ca10be6400b9365face63c15361dff679af80
Hirokai Bandai <totemham.4230@gmail.com>
2020-09-27 20:38:00 +0900

    Updates

---
+++ precache-manifest.94ce3e17b5e47e80c6cb4d939fa1aab2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83393e41bc36d15ee5d2ca49752521a3",
+    "url": "/Myportfolio_with_React/index.html"
+  },
+  {
+    "revision": "08c54358da66370e2798",
+    "url": "/Myportfolio_with_React/static/css/main.06f9181e.chunk.css"
+  },
+  {
+    "revision": "79074ced895e94e3cca7",
+    "url": "/Myportfolio_with_React/static/js/2.2ee7f0d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Myportfolio_with_React/static/js/2.2ee7f0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c54358da66370e2798",
+    "url": "/Myportfolio_with_React/static/js/main.8472f389.chunk.js"
+  },
+  {
+    "revision": "ab777b38ea502cc13a62",
+    "url": "/Myportfolio_with_React/static/js/runtime-main.50aee2e0.js"
+  },
+  {
+    "revision": "fda363107aad8cb5faedcc6114c2e821",
+    "url": "/Myportfolio_with_React/static/media/Hiroaki.fda36310.JPG"
+  },
+  {
+    "revision": "32278fbb8de7b88f892fdc27da589aca",
+    "url": "/Myportfolio_with_React/static/media/contactimage.32278fbb.jpg"
+  },
+  {
+    "revision": "3747f652ad3825a675e3d9c33ded4bd5",
+    "url": "/Myportfolio_with_React/static/media/logo1.3747f652.png"
+  },
+  {
+    "revision": "664aa4a1d32f64d4e2c2652ac6d7cf47",
+    "url": "/Myportfolio_with_React/static/media/mainImage.664aa4a1.jpg"
+  }
+]);
